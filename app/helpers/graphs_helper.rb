70963671cd600e6164240697c69cc519e768ad99
jbbarth <jeanbaptiste.barth@gmail.com>
2010-07-28 14:29:55 +0200

    Moved jqplot graphs HTML in the partial

--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -1,18 +1,2 @@
 module GraphsHelper
-  def jqplot_graph(graph)
-    return unless graph.is_a?(Graph)
-    html = []
-    html << %(<div class="jqPlot" id="chart#{graph.id}" style="height:300px;width:450px;"></div>)
-    html << %(<script type="text/javascript" language="javascript">)
-    html << %(//<![CDATA[)
-    html << %(jQuery.noConflict();)
-    html << %(line#{graph.id} = #{graph.eval_source};)
-    html << "plot#{graph.id} = jQuery.jqplot('chart#{graph.id}', [line#{graph.id}], {"
-    html << %(  title: '#{graph.title}',)
-    html << %(  #{graph.rendering.split("\n").join("  \n")})
-    html << "});"
-    html << %(//]]>)
-    html << "</script>"
-    html.join("\n")
-  end
 end
