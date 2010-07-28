77007c3a9d6054c74bbec137c6f901587fb1ac6f
jbbarth <jeanbaptiste.barth@gmail.com>
2010-07-28 11:53:06 +0200

    Added some unit tests to graph model

--- test/unit/graph_test.rb
+++ test/unit/graph_test.rb
@@ -2,9 +2,14 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class GraphTest < ActiveSupport::TestCase
   fixtures :graphs
-
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  
+  def test_eval_source
+    data = "[['frogs',3],['buzzards',7],['deer',2.5],['turkeys',6],['moles',5],['ground hogs',4]]"
+    assert_equal data, Graph.find(1).eval_source
+    assert_equal data.gsub("'",'"'), Graph.find(2).eval_source
+  end
+  
+  def test_renderers
+    assert_equal ["plugins/jqplot.pieRenderer.js"], Graph.find(1).renderers
   end
 end
